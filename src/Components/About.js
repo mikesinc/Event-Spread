33b23faabbfa40d05882a7098bd869fba904e589
mikesinc <mas.sinclair@gmail.com>
2019-08-19 22:38:14 +0800

    changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './About.css';
 
 const About = () => {
     return(
-        <h1>About</h1>
+        <Container fluid className="aboutBack">
+            <h1>About</h1>
+            <h2>Special Event Decorative Food Platters</h2>
+        </Container>
     )
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,13 +4,18 @@ import Nav from 'react-bootstrap/Nav';
 import './Navbar.css'
 
 const NavBar = () => {
+
+    // const setTop = (height) => {
+    //     window.scrollY = height;
+    // }
+
     return (
         <Navbar fixed='top' bg="light" expand="sm" variant="light">
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                     <Nav.Link className="px-5" href="/">home</Nav.Link>
-                    <Nav.Link className="px-5" href="/about">about</Nav.Link>
+                    {/* <Nav.Link className="px-5" onClick={() => setTop(50)}>about</Nav.Link> */}
                     <Nav.Link className="px-5" href="/contact">contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
