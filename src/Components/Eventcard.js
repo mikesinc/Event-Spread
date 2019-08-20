7fb4fce03745d9b912ef9b593fe984634255e552
mikesinc <mas.sinclair@gmail.com>
2019-08-20 23:29:46 +0800

    majority front end

--- src/Components/Eventcard.js
+++ src/Components/Eventcard.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import './Eventcard.css'
 
-const Eventcard = ({ event, text }) => {
+const Eventcard = ({ event, text, left, right }) => {
     return (
-        <Container fluid className='card'>
+        <Container fluid className='card' style={{ paddingLeft: `${left}%`, paddingRight: `${right}%` }} >
             <h2>{event}</h2>
-            <img className='icon' alt='bday' src={require('../assets/images/spread1.jpg')}></img>
             <p>{text}</p>
+            <img style={{ right: left === '25' ? '77%' : '3%' }} className='icon' alt='bday' src={require(`../assets/images/${event}.jpg`)}></img>
         </Container>
     )
 }
